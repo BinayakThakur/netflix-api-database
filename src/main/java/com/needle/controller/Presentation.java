9b630526963745076cd3625590d2958eded3d434
Binayak Thakur <thakurganesh919@gmail.com>
2022-04-10 17:01:08 +0530

    added cors changes

--- src/main/java/com/needle/controller/Presentation.java
+++ src/main/java/com/needle/controller/Presentation.java
@@ -1,6 +1,5 @@
 package com.needle.controller;
 
-
 import com.needle.utlis.SearchMapper;
 import com.needle.utlis.SearchUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,43 +16,47 @@ import java.util.stream.Collectors;
 @RestController
 public class Presentation {
     /*
-        Initializing the search algo is two step process and is generic to any entity
-        1)There should be a entity manger that needs to be autowired
-        2)Now Search Utils can be AutoWired to provide helper functions
+     * Initializing the search algo is two step process and is generic to any entity
+     * 1)There should be a entity manger that needs to be autowired
+     * 2)Now Search Utils can be AutoWired to provide helper functions
      */
-    @Autowired EntityManager em;
-    @Autowired SearchUtils searchUtils;
+    @Autowired
+    EntityManager em;
+    @Autowired
+    SearchUtils searchUtils;
 
     /*
-    Now the algorithm can simply be used as follows
-    Algo have two functions
-    searchMultiFieldQuery which takes, table name, n field items ,n search items
-    searchMultiFieldQueryOr which takes, table name, n field items ,n search items
-    Sort is implemented using first field element
-    */
+     * Now the algorithm can simply be used as follows
+     * Algo have two functions
+     * searchMultiFieldQuery which takes, table name, n field items ,n search items
+     * searchMultiFieldQueryOr which takes, table name, n field items ,n search
+     * items
+     * Sort is implemented using first field element
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
 
     @PostMapping("/search_and")
-    public @ResponseBody List searchDataAnd(@RequestBody SearchMapper map){
+    public @ResponseBody List searchDataAnd(@RequestBody SearchMapper map) {
 
-        List data= em.createNativeQuery
-                (searchUtils.searchMultiFieldQuery(map.getTable(), map.getFields(),map.getSearch()))
+        List data = em
+                .createNativeQuery(searchUtils.searchMultiFieldQuery(map.getTable(), map.getFields(), map.getSearch()))
                 .getResultList();
 
         return data;
     }
+
     @PostMapping("/search_or")
-    public @ResponseBody List searchDataOr(@RequestBody SearchMapper map){
+    public @ResponseBody List searchDataOr(@RequestBody SearchMapper map) {
 
-        List data= em.createNativeQuery
-                (searchUtils.searchMultiFieldQueryOr(map.getTable(), map.getFields(),map.getSearch())).getResultList();
+        List data = em
+                .createNativeQuery(
+                        searchUtils.searchMultiFieldQueryOr(map.getTable(), map.getFields(), map.getSearch()))
+                .getResultList();
         return data;
-    }
-
-
-
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 
 }
